e61c864a26deef444b54c00f4858ab29ce508975
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-09-25 21:37:59 +0900

    feat(servlet): RequestHeaderServlet에 헤더 편의 조회 기능 추가

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.request;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,6 +18,7 @@ public class RequestHeaderServlet extends HttpServlet {
         // Start Line 정보 불러오기
         printStartLine(request);
         printHeaders(request);
+        printHeaderUtils(request);
 
     }
 
@@ -82,6 +84,55 @@ public class RequestHeaderServlet extends HttpServlet {
         accept-language = accept-language
         cookie = cookie
         --- Headers - end ---
+         */
+    }
+
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("--- Header 편의 조회 start ---");
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " + request.getServerName()); // Host 헤더
+        System.out.println("request.getServerPort() = " + request.getServerPort()); // Host 헤더
+        System.out.println();
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        System.out.println("request.getLocale() = " + request.getLocale());
+        System.out.println();
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+        System.out.println();
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " + request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
+        System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+
+        /*
+        --- Header 편의 조회 start ---
+        [Host 편의 조회]
+        request.getServerName() = localhost
+        request.getServerPort() = 8080
+
+        [Accept-Language 편의 조회]
+        locale = ko_KR
+        locale = ko
+        locale = en_US
+        locale = en
+        request.getLocale() = ko_KR
+
+        [cookie 편의 조회]
+        JSESSIONID: 3C011FA5B1E74348DA237197DA296731
+
+        [Content 편의 조회]
+        request.getContentType() = null
+        request.getContentLength() = -1
+        request.getCharacterEncoding() = UTF-8
+        --- Header 편의 조회 end ---
          */
     }
 }
